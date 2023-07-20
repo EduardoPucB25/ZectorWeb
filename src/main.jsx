81237b58d0c948077b12b7c84547013097ed31c2
EduardoP-BF25 <105117644+EduardoP-BF25@users.noreply.github.com>
2023-07-19 18:25:37 -0600

    Modificacion!! Julio 19

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-
-// Importar los css
-// import '../src/assets/css/admin/header.css'
- 
 import 'primeicons/primeicons.css';
-
-        
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
