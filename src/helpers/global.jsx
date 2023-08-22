ef5b4ae50e2abe93a8043f900b0d5e6b7ebe31a0
EduardoP-BF25 <105117644+EduardoP-BF25@users.noreply.github.com>
2023-08-22 14:35:54 -0600

    Update 22 Agosto

--- src/helpers/global.jsx
+++ src/helpers/global.jsx
@@ -1,5 +1,6 @@
 export const Global = {
-   url: "http://zectorapi-production.up.railway.app/zector/"                                                  
+   url: "zectorapi-production.up.railway.app/zector/"   
+   
    // url: "http://localhost:3900/zector/",                                                   
 
    
